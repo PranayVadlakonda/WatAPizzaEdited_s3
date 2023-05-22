affd623c51248cb8bc5768a73cf1588473401752
pranay <pranayanand61@gmail.com>
2023-05-22 21:37:39 +0530

    prompt the customer name

--- src/main/java/com/niit/com/PizzaOrder.java
+++ src/main/java/com/niit/com/PizzaOrder.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class PizzaOrder {
     public static void main(String[] args) {
+        // initialize scanner class
         Scanner Scanner = new Scanner(System.in);
+
+        // prompt the customer name
+        String customerName = input.nextLine();
+
+        // prompt the customer email
 
 
     }
