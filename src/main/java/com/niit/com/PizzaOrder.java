37654fcfdc8e5edffaf18f531765d8b506d0dfc9
pranay <pranayanand61@gmail.com>
2023-05-22 21:34:01 +0530

    initialize scanner class

---
+++ src/main/java/com/niit/com/PizzaOrder.java
@@ -0,0 +1,11 @@
+package com.niit.com;
+
+import java.util.Scanner;
+
+public class PizzaOrder {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+
+
+    }
+}
